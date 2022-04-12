8550dae56da1c09e6922fcd2dbf3a7cefcfa3809
MangKyu <whalsrb1226@naver.com>
2022-04-13 02:56:04 +0900

    fix: Randoms에서 pickNumberInRange를 사용하도록 변경한다.

--- src/main/java/baseball/app/ball/generator/AutoBallsGenerator.java
+++ src/main/java/baseball/app/ball/generator/AutoBallsGenerator.java
@@ -3,7 +3,10 @@ package baseball.app.ball.generator;
 import baseball.app.ball.Balls;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import static baseball.constants.GameConstants.*;
 
@@ -11,8 +14,17 @@ public class AutoBallsGenerator extends AbstractBallsGenerator {
 
     @Override
     public Balls generate() {
-        final List<Integer> integerList = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, MAX_BALL_COUNT);
+        final List<Integer> integerList = createUniqueIntegerList();
         return createBalls(integerList);
+    }
+
+    private List<Integer> createUniqueIntegerList() {
+        final Set<Integer> set = new LinkedHashSet<>(MAX_BALL_COUNT);
+        while (set.size() != MAX_BALL_COUNT) {
+            final int number = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            set.add(number);
+        }
+        return new ArrayList<>(set);
     }
 
 }
